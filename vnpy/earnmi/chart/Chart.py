1c6222b01ef2dc631af64a939880c647a9973681
raodongming@hotmail.com <raoming1987>
2020-06-09 21:36:00 +0800

    添加图表功能

--- vnpy/earnmi/chart/Chart.py
+++ vnpy/earnmi/chart/Chart.py
@@ -8,28 +8,72 @@ import numpy as np
 from vnpy.trader.constant import Interval, Exchange
 from vnpy.trader.database import database_manager
 from vnpy.trader.object import BarData
+from vnpy.trader.utility import ArrayManager
+
 
 class Chart:
 
+
+    window_size = 15
+    open_boll = False  ##是否显示布林指标
 
 
     """
     设置数据
     """
     def setBarData(self,bars:list) :
-       data = []
-       index = []
-       for bar in bars:
-           index.append(bar.datetime)
-           data.append([bar.open_price, bar.high_price, bar.low_price, bar.close_price, bar.volume])
-       trades = pd.DataFrame(data, index=index, columns=['Open', 'High', 'Low', 'Close', "Volume"])
-       self.data = trades
+       self.barDatas = bars
+
 
 
     """
     显示图表
     """
     def show(self):
-        mpf.plot(self.data, type='candle', volume=True, mav=(5), figscale=0.9, style='yahoo')
-        pass
+        bars = self.barDatas;
+        if(bars[0].datetime > bars[-1].datetime):
+            bars = bars.__reversed__()
+        print(bars)
+        data = []
+        index = []
+        am = ArrayManager(self.window_size * 2)
+
+        ### 初始化columns
+        columns = ['Open', 'High', 'Low', 'Close', "Volume"]
+
+        enable_bool = self.open_boll
+        if enable_bool:
+            columns.append("boll_up")
+            columns.append("boll_down")
+
+        for bar in bars:
+            index.append(bar.datetime)
+            list = [bar.open_price, bar.high_price, bar.low_price, bar.close_price, bar.volume]
+            am.update_bar(bar)
+
+            #添加布林指标数据
+            if enable_bool:
+                if am.count >= self.window_size:
+                    up, down = am.boll(self.window_size, 3.4)
+                    list.append(up)
+                    list.append(down)
+                else:
+                    list.append(bar.close_price)
+                    list.append(bar.close_price)
+
+            data.append(list)
+
+
+        trades = pd.DataFrame(data, index=index, columns=columns)
+
+        apds = []
+
+        # 添加布林指标数据
+        if enable_bool:
+            apds.append(mpf.make_addplot(trades['boll_up'], linestyle='dashdot'))
+            apds.append(mpf.make_addplot(trades['boll_down'], linestyle='dashdot'))
+
+
+        mpf.plot(trades, type='candle', volume=True, mav=(5), figscale=0.9, style='yahoo',addplot=apds)
+
 
--- vnpy/earnmi_demo/iiiii.py
+++ vnpy/earnmi_demo/iiiii.py
@@ -11,21 +11,44 @@ from vnpy.trader.object import BarData
 import jqdatasdk as jq
 import numpy as np
 
+from vnpy.trader.utility import ArrayManager
+
 symbol = "600009";
-bars = database_manager.load_bar_data(symbol,Exchange.SSE,Interval.DAILY,datetime(2018,4,1),datetime(2020,3,25))
+bars = database_manager.load_bar_data(symbol,Exchange.SSE,Interval.DAILY,datetime(2019,4,1),datetime(2020,3,25))
+bars.__reversed__()
 data = []
 index=[]
-for bar in bars:
-    index.append(bar.datetime)
-    data.append([bar.open_price,bar.high_price,bar.low_price,bar.close_price,bar.volume])
+am = ArrayManager()
+boll_window = 18
+boll_dev = 3.4
 
-data = pd.DataFrame(data,index=index,columns=['Open','High','Low','Close',"Volume"])
-#print(data)
+# for bar in bars:
+#     am.update_bar(bar)
+#     if am.count >= boll_window:
+#         up,down = am.boll(boll_window,boll_dev)
+#     else:
+#         up,down = [bar.close_price,bar.close_price]
+#     index.append(bar.datetime)
+#     data.append([bar.open_price,bar.high_price,bar.low_price,bar.close_price,bar.volume,up,down])
+#
+#
+#
+#
+# data = pd.DataFrame(data,index=index,columns=['Open','High','Low','Close',"Volume","Boll_Up","Boll_Down"])
+#
+# #print(data)
+# apds = [ mpf.make_addplot(data['Boll_Up'],linestyle='dashdot'),
+#          mpf.make_addplot(data['Boll_Down'], linestyle='dashdot'),
+#        ]
+#
+#
+# mpf.plot(data, type='candle', volume=True, mav=(5), figscale=0.9, style='yahoo',addplot=apds)
 
-chart = Chart()
-
-chart.setBarData(bars)
-chart.show()
+char = Chart()
+char.setBarData(bars)
+char.open_boll = True
+#char.openBoll(False)
+char.show()
 
 
 
