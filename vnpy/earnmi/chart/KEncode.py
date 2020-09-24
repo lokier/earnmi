df028a788c8d0cd357b06ed3f04b31c7dc9d1ef0
饶东铭 <11101037@vivo.xyz>
2020-09-24 11:06:19 +0800

    完善KPattern

--- vnpy/earnmi/chart/KEncode.py
+++ vnpy/earnmi/chart/KEncode.py
@@ -6,16 +6,17 @@
 class KEncode():
 
 
-    def encodeAlgro1(pre_close:float,open:float,high:float,low:float,close:float) ->int:
-        ret, pct_code, high_extra_pct_code, low_extra_pct_code = KEncode.__parseAlgro1(pre_close,open,high,low,close);
+    def encodeAlgro1(pre_close:float,open:float,high:float,low:float,close:float
+                     ,pct_split = [-6, -3, -1.5, -0.5, 0.5, 1.5, 3, 5.5]
+                     ,extra_split = [1, 2.5]
+                     ) ->int:
+        ret, pct_code, high_extra_pct_code, low_extra_pct_code = KEncode.parseAlgro1(pre_close,open,high,low,close,pct_split,extra_split);
         return ret
     """
     值范围为：0-80（小于81）
     （坏处：没有把开盘价考虑进去）
     """
-    def __parseAlgro1(pre_close:float,open:float,high:float,low:float,close:float
-                      ,pct_split=[-6.7,-4.1,-2.0,-0.5,0.5,2.0,4.1,6.7]
-                      ,extra_split=[0.5,0.7]):
+    def parseAlgro1(pre_close:float,open:float,high:float,low:float,close:float,pct_split:[],extra_split:[]):
 
         pct_size = len(pct_split)
         extra_size = len(extra_split)
@@ -25,37 +26,28 @@ class KEncode():
         #low_extra_pct = 100 * (min(open, close) - low) / pre_close
 
         pct_code = 0
-        for i in range(0,pct_size):
-            if pct > pct_split[i]:
+        for i in range(pct_size,0,-1):
+            if pct > pct_split[i-1]:
                 pct_code = i
-        if pct_code ==-1:
-            pct_code = pct_size - 1
+                break
 
-        high_extra_pct_code = -1
-        high_extra_pct =abs(high - max(open, close)) / pre_close
-        for i in range(0, extra_size - 1):
-            if high_extra_pct < extra_split[i]:
-                high_extra_pct_code = i
-        if high_extra_pct_code == -1:
-            high_extra_pct_code = extra_size - 1
-
-        pct_length = abs(open - close) #3个值
-
-        low_extra_length = abs(min(close,open)-low)
         high_extra_pct_code = 0
-        if high_extra_length > pct_length:
-            if high_extra_length < 2.5 * pct_length:
-                high_extra_pct_code = 1
-            else:
-                high_extra_pct_code = 2
+        high_extra_pct = 100 * abs(high - max(open, close)) / pre_close
+        for i in range(extra_size,0,-1):
+            if high_extra_pct > extra_split[i-1]:
+                high_extra_pct_code = i
+                break
 
-        low_extra_pct_code = 0  #3个值
-        if low_extra_length > pct_length:
-            if low_extra_length < 2.5 * pct_length:
-                low_extra_pct_code = 1
-            else:
-                low_extra_pct_code = 2
-        ret = low_extra_pct_code * 3* 9 + high_extra_pct_code * 9 + pct_code
+        low_extra_pct_code = 0
+        low_extra_pct = 100 * abs(min(open, close) - low) / pre_close
+        for i in range(extra_size,0,-1):
+            if low_extra_pct > extra_split[i-1]:
+                low_extra_pct_code = i
+                break
+
+        PCT_MASK = pct_size + 1
+        EXTRA_PCT_MASK = extra_size + 1
+        ret = low_extra_pct_code * EXTRA_PCT_MASK* PCT_MASK + high_extra_pct_code * PCT_MASK + pct_code
         return ret,pct_code,high_extra_pct_code,low_extra_pct_code
 
 
--- vnpy/earnmi_demo/strategy_demo/holdbars/analysisKPattern.py
+++ vnpy/earnmi_demo/strategy_demo/holdbars/analysisKPattern.py
@@ -8,6 +8,7 @@ import math
 from datetime import datetime
 import numpy as np
 from earnmi.chart.Indicator import Indicator
+from earnmi.chart.KEncode import KEncode
 from earnmi.chart.KPattern import KPattern
 
 
@@ -59,16 +60,19 @@ def compute_SW_KPattern_data():
                         dataItem.pcts = []   ##识别之后第二天的盈利情况
                         dataSet[name] = dataItem
                     ##第二天的收益
-                    pct = (bar.close_price - preBar.close_price) / preBar.close_price
+                    short_pct = ((bar.high_price + bar.close_price) / 2 - preBar.close_price) / preBar.close_price
+                    #long_pct = ((bar.high_price + bar.close_price) / 2 - preBar.close_price) / preBar.close_price
+
+                    ###pct = (bar.close_price - preBar.close_price) / preBar.close_price
 
                     ##收录当前形态
                     dataItem.count_total += 1
-                    dataItem.pct_total += pct
-                    if pct > 0.000001:
+                    dataItem.pct_total += short_pct
+                    if short_pct > 0.000001:
                         dataItem.count_earn += 1
-                        dataItem.pct_earn += pct
+                        dataItem.pct_earn += short_pct
                     dataItem.values.append(value)
-                    dataItem.pcts.append(pct)
+                    dataItem.pcts.append(short_pct)
 
                 pass
             indicator.update_bar(bar)
@@ -181,7 +185,7 @@ def compute_SW_KEncode_data():
                 dataItem = CountItem()
                 dataSet[kEncodeValue] = dataItem
             ##第二天的收益
-            pct = (bar.close_price - preBar.close_price) / preBar.close_price
+            pct = ((bar.high_price + bar.close_price) / 2 - preBar.close_price) / preBar.close_price
             ##收录当前形态
             #dataItem.values.append(value)
             dataItem.count_total +=1
@@ -211,12 +215,69 @@ def compute_SW_KEncode_data():
 
 
 """
-统计申万行业的k线编码识别。
+计算KEncode_parseAlgro1的分割在sw的动态分布情况
 """
-def compute_SW_KEncode_data():
+def compute_SW_KEncode_parseAlgro1_split(
+        pct_split = [-6, -3, -1.5, -0.5, 0.5, 1.5, 3, 5.5]
+        ,extra_split = [1, 2.5]):
+    from earnmi.data.SWImpl import SWImpl
+    sw = SWImpl()
+    lists = sw.getSW2List()
+    start = datetime(2014, 5, 1)
+    end = datetime(2020, 8, 17)
+    dataSet = {}
 
-    pass
+    total_count = 0
+    pct_code_count = np.zeros(9)
+    high_extra_pct_code_count = np.zeros(3)
+    low_extra_pct_code_count = np.zeros(3)
+
+    for code in lists:
+        #for code in lists:
+        barList = sw.getSW2Daily(code, start, end)
+        indicator = Indicator(40)
+        preBar = None
+        for i in range(0,len(barList)):
+            bar = barList[i]
+            indicator.update_bar(bar)
+
+            if (i > 10):
+                k_code,pct_code,high_extra_pct_code,low_extra_pct_code = KEncode.parseAlgro1(indicator.close[-2], indicator.open[-1], indicator.high[-1],
+                                              indicator.low[-1], indicator.close[-1],pct_split,extra_split)
+                high_extra_pct_code_count[high_extra_pct_code] =  high_extra_pct_code_count[high_extra_pct_code] + 1
+                low_extra_pct_code_count[low_extra_pct_code] =  low_extra_pct_code_count[low_extra_pct_code] + 1
+                pct_code_count[pct_code] =  pct_code_count[pct_code] + 1
+                total_count += 1
+            preBar = bar
+
+        ##打印当前形态
+    print(f"pct_split: {pct_split}")
+    print(f"extra_split: {extra_split}")
+
+    count_list = pct_code_count
+    print(f"应该服从正太分布" )
+    print(f"\n：pct_code_count分布,avg = %.4f%%" % ( 100 / len(count_list)))
+    for codeId in range(0,len(count_list)):
+        item_count = count_list[codeId]
+        item_occur_rate = 100 * item_count / total_count
+        print(f"\t{codeId}: %.4f  %%   count:%d" % (item_occur_rate,item_count))
+
+    count_list = high_extra_pct_code_count
+    print(f"\n：high_extra_pct_code_count分布,avg = %.4f%%" % ( 100 / len(count_list)))
+    for codeId in range(0, len(count_list)):
+        item_count = count_list[codeId]
+        item_occur_rate = 100 * item_count / total_count
+        print(f"\t{codeId}: %.4f  %%   count:%d" % (item_occur_rate,item_count))
+
+    count_list = low_extra_pct_code_count
+    print(f"\n：low_extra_pct_code_count分布,avg = %.4f%%" % ( 100 / len(count_list)))
+    for codeId in range(0, len(count_list)):
+            item_count = count_list[codeId]
+            item_occur_rate =100 *  item_count / total_count
+            print(f"\t{codeId}: %.4f  %%   count:%d" % (item_occur_rate,item_count))
+
 
 if __name__ == "__main__":
     #compute_SW_KPattern_data()
     compute_SW_KEncode_data()
+    #compute_SW_KEncode_parseAlgro1_split();
