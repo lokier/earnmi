0ded34cf0629f4584a26da9ee8e90ec124b3ab40
饶东铭 <11101037@vivo.xyz>
2020-10-19 20:36:26 +0800

    添加kdj_movement策略。

--- vnpy/earnmi_demo/show_chart_demo.py
+++ vnpy/earnmi_demo/show_chart_demo.py
@@ -7,7 +7,7 @@ from werkzeug.routing import Map
 from earnmi.chart.Chart import Chart, BollItem, IndicatorItem, Signal, HoldBarMaker
 from earnmi.chart.Indicator import Indicator
 from earnmi.data.SWImpl import SWImpl
-
+from earnmi.model.BarDataSource import ZZ500DataSource
 
 """
  RSI指标
@@ -37,6 +37,46 @@ class IndicatorLine(IndicatorItem):
     def isLowerPanel(self) ->bool:
         return True
 
+"""
+ RSI指标
+"""
+class kdj(IndicatorItem):
+
+    maker:HoldBarMaker = HoldBarMaker()
+    def __init__(self):
+        super().__init__(False)
+
+    def getNames(self) -> List:
+        return ["k","d","j"]
+
+
+    def getValues(self, indicator: Indicator,bar:BarData,signal:Signal) -> Map:
+        values = {}
+        if indicator.count>20:
+            k,d,j = indicator.kdj(fast_period=9,slow_period=3,array=True)
+            values["k"] = k[-1]
+            values["d"] = d[-1]
+            values["j"] = j[-1]
+
+            if k[-2] < d[-2] and k[-1] >= d[-1]:
+                signal.buy = True
+
+        else:
+            values["k"] = 50
+            values["d"] = 50
+            values["j"] = 50
+        return values
+
+    def getColor(self, name: str):
+        if name == "k":
+            return 'r'
+        elif name == 'd':
+            return 'b'
+        return 'y'
+
+    def isLowerPanel(self) ->bool:
+        return True
+
 code = "600196"
 
 start = datetime(2020, 5, 1)
@@ -50,20 +90,16 @@ end = datetime.now();
 #market.setToday(datetime.now())
 #bars = market.getHistory().getKbarFrom(code,start)
 
-sw = SWImpl()
-codeList = sw.getSW2List()
-code = codeList[1]
-start = datetime(2020, 5, 1)
-bars = sw.getSW2Daily(code,start,end)
 
+source = ZZ500DataSource(start,end)
 
-for code in codeList:
-    print(f"name = {sw.getSw2Name(code)},len = {len(sw.getSW2Stocks(code))}")
+bars,code = source.onNextBars();
+
 
 print(f"bar.size = {bars.__len__()}")
 
 
 chart = Chart()
 
-chart.show(bars,IndicatorLine())
+chart.show(bars,kdj())
 #chart.showCompare(bars,"000300")
