ea4664ef36ee9b1fe6b97f1ab0977949ed71f1ec
raodongming@hotmail.com <raoming1987>
2021-01-22 09:18:35 +0800

    修正bug

--- vnpy/earnmi/core/Context.py
+++ vnpy/earnmi/core/Context.py
@@ -37,7 +37,7 @@ class Context:
         """
         是否在回测环境下运行。
         """
-        return self.is_backtest()
+        return self.engine.is_backtest()
 
     def is_mainThread(self) -> bool:
         """
